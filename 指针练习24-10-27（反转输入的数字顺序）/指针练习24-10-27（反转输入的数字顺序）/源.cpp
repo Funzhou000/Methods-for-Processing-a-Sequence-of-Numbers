225de58aa57b6594063f62f4bf8620d4d96103e0
FUNZHOU <130201517+Funzhou000@users.noreply.github.com>
2024-10-27 20:23:30 +0800

    Add files via upload
    基于指针的交换数字。让数字反转输出

---
+++ 指针练习24-10-27（反转输入的数字顺序）/指针练习24-10-27（反转输入的数字顺序）/源.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int a,n;
+int arr[100000];
+int main() {
+	cout << "请输入数字个数";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{   cin >> a;
+	    arr[i] = a;
+	}
+	int* start = arr;
+	int* end = arr + n - 1;
+	while (start < end) {
+		int temporary = *start;
+		    *start=*end;//左边为储存位置//前一个版本只让end变为和start的值，start仍然不变；（！！注意交换赋值一定要两边都换）
+			*end=temporary;
+			start++; end--;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	return 0;
+}
