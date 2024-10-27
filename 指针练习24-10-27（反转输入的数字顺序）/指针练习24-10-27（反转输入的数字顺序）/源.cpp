8dc1a924930812a231660405c4d4cf52a7434899
FUNZHOU <130201517+Funzhou000@users.noreply.github.com>
2024-10-27 20:35:03 +0800

    Add files via upload
    这是反转输入的数字基于指针+函数的版本

--- 指针练习24-10-27（反转输入的数字顺序）/指针练习24-10-27（反转输入的数字顺序）/源.cpp
+++ 指针练习24-10-27（反转输入的数字顺序）/指针练习24-10-27（反转输入的数字顺序）/源.cpp
@@ -3,13 +3,7 @@
 using namespace std;
 int a,n;
 int arr[100000];
-int main() {
-	cout << "请输入数字个数";
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{   cin >> a;
-	    arr[i] = a;
-	}
+void reverseArray(int* start, int* end) {
 	int* start = arr;
 	int* end = arr + n - 1;
 	while (start < end) {
@@ -18,6 +12,23 @@ int main() {
 			*end=temporary;
 			start++; end--;
 	}
+
+}
+int main() {
+	cout << "请输入数字个数";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{   cin >> a;
+	    arr[i] = a;
+	}
+	//int* start = arr;
+	//int* end = arr + n - 1;
+	//while (start < end) {
+	//	int temporary = *start;
+	//	    *start=*end;//左边为储存位置//前一个版本只让end变为和start的值，start仍然不变；（！！注意交换赋值一定要两边都换）
+	//		*end=temporary;
+	//		start++; end--;
+	//}
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << " ";
