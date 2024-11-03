91b039da142e374f74326175a5e733d2d170343a
FUNZHOU <130201517+Funzhou000@users.noreply.github.com>
2024-11-03 11:59:01 +0800

    Add files via upload

---
+++ 检测数字位数/检测数字位数/源.cpp
@@ -0,0 +1,20 @@
+#define  _CRT_SECURE_NO_WARNINGS
+
+//#include <iostream> // 标准输入输出流
+//#include <vector>   // 标准模板库中的向量
+//#include <string>   // C++ 字符串类
+//#include <cmath>    // 数学函数
+
+//int main() {
+//    int a;
+//    int n = 0;
+//    printf("输入数字");
+//    scanf("%d", &a);
+//    while ( a>=1) {
+//        a=a/10; n++;
+//    }
+//    printf("位数是%d", n);
+//    return 0;
+//}
+  // 数学函数
+
---
+++ 检测数字位数/检测数字位数/源1.cpp
@@ -0,0 +1,27 @@
+#include <iostream> // 标准输入输出流
+#include <vector>   // 标准模板库中的向量
+#include <string>   // C++ 字符串类
+#include <cmath>    // 数学函数
+using namespace std;
+
+int main() {
+	int a , Type , n ;
+
+	cin >> a;
+	Type = sizeof(a);
+	switch (Type)
+	{
+	case 1:
+		n = 1;
+		break;
+	case 2:
+		n = 2;
+		break;
+	default:
+		cout <<Type;
+		break;
+
+	}
+
+    return 0;
+}
