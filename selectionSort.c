816aba901ed36e8b7ba8c966f5facb76e06644f2
FUNZHOU <130201517+Funzhou000@users.noreply.github.com>
2024-12-03 23:47:55 +0800

    Update selectionSort.c
    add description

--- selectionSort.c
+++ selectionSort.c
@@ -2,7 +2,7 @@
 void selectionSort(int arr[],int n){
     for(int i = 0;i<n-1;i++){
         int min_idx=i;
-        for(int j=i+1;j<n;j++){
+        for(int j=i+1;j<n;j++){//查找i后面的数字是否有比mini_index所指向的数字小；
             if(arr[j]<arr[min_idx]){
                 min_idx=j;
             }
@@ -13,3 +13,4 @@ void selectionSort(int arr[],int n){
     }
 
 }
+
